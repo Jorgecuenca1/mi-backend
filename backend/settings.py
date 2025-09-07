7753b0830d9c608bbe3670558a69689c05b9f681
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2025-09-07 18:04:31 -0500

    Kk

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-b0^2d&m3zu$cl6vx!6v*ye5^k&)*nddo$=m!_wxwl15(xwjprz
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['*','corpofuturo.org','seguroslavictoria.co', "https://cuentas.corpofuturo.org", "vacunacion.corpofuturo.org",'cuentasmedicas.seguroslavictoria.co','vacunacion.solucionesfinancierasglobal.com','cuentasmedicas.corpofuturo.org','https://cuentasmedicas.seguroslavictoria.co','https://vacunacion.corpofuturo.org']
+ALLOWED_HOSTS = ['*','corpofuturo.org','seguroslavictoria.co', "https://cuentas.corpofuturo.org", "vacunacion.corpofuturo.org",'cuentasmedicas.seguroslavictoria.co','vacunacion.solucionesfinancierasglobal.com','vacuna.corpofuturo.org','https://vacuna.corpofuturo.org','https://vacunacion.corpofuturo.org']
 
 
 
@@ -80,6 +80,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5000",      # Flutter Web (ajusta el puerto real)
     "http://localhost:5173",
 "https://vacunacion.corpofuturo.org",
+"https://vacuna.corpofuturo.org",
     "https://vacunacion.solucionesfinancierasglobal.com",
 ]
 
@@ -90,7 +91,7 @@ CSRF_TRUSTED_ORIGINS = [
         "http://localhost:5000",      # Flutter Web (ajusta el puerto real)
         "http://localhost:5173",
     "https://corpofuturo.org",
-    "https://cuentasmedicas.corpofuturo.org",
+    "https://vacuna.corpofuturo.org",
 "https://cuentasmedicas.seguroslavictoria.co",
     "https://seguroslavictoria.co",
     "https://vacunacion.corpofuturo.org",
